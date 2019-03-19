94b8fb91ef7d4f68af83419e88b46896d2c380f7
Michael Renken <michaelarenken@gmail.com>
2019-03-19 16:07:28 -0500

    Added backend API call and plant cards to browser

--- src/Browser.js
+++ src/Browser.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
+import {
+    Card,
+    CardImg,
+    CardColumns,
+    CardBody,
+    CardTitle,
+    CardText,
+} from 'reactstrap';
+
+const settings = require("./settings.json")
 
 class Browser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "categories": [],
+            "plants": [],
+        };
+
+    }
+
+    componentDidMount() {
+        fetch(settings.backendServer + "/v1/categories/").then((resp) => {
+            return resp.json();
+
+        }).then((data) => {
+            this.setState({"categories": data});
+
+        });
+
+        fetch(settings.backendServer + "/v1/plants/").then((resp) => {
+            return resp.json();
+
+        }).then((data) => {
+            console.log(data);
+            this.setState({"plants": data});
+
+        });
+
+    }
 
     render() {
-        return <div />;
+        return (
+            <CardColumns>
+                {
+                    this.state.plants.map((plant) => {
+                        return (
+                            <Card key={plant.name}>
+                                {
+                                    plant.image ? (
+                                        <CardImg src={plant.image} />
+                                    ) : ""
+                                }
+                                <CardBody>
+                                    <CardTitle><h3>{plant.name}</h3></CardTitle>
+                                    <CardText>{plant.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        );
+
+                    })
+
+                }
+            </CardColumns>
+
+        );
 
     }
 
